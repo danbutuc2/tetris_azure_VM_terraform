2bc49f884ad79f48e7ab2fee793ea75ace87880c
Dan Butuc <danbutuc@gmail.com>
2025-04-30 09:26:51 +0200

    updating the backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-/*terraform {
+terraform {
   backend "azurerm" {
     resource_group_name   = "danResourceGroup" 
     storage_account_name  = "tfstatestoragedan"
     container_name        = "tfstate-container" 
     key                   = "terraform.tfstate"  
   }
-}*/
+}
